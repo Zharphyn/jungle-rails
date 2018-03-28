5e4bb5bce3fdd279b5783a07ce8e8f4e26be2580
Zharphyn <brad@zharphyn.ca>
2018-03-28 22:29:12 +0000

    development continues

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,4 @@
-class Admin::ProductsController < ApplicationController
-
+class Admin::ProductsController < Admin::BaseAdminController
   def index
     @products = Product.order(id: :desc).all
   end

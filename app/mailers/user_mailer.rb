a5b60927abcd9cacb6859173df23b45a83f8042c
Zharphyn <brad@zharphyn.ca>
2018-03-29 02:49:33 +0000

    Adding classes to aid with email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+  def order_complete(order)
+    @order = order
+    @user = @order.user
+    @url = "http://0.0.0.0:3000/order/#{order.id}"
+    mail(to: @user.email, subject: "Order complete! (order number: #{order.id})")
+  end
+
+end
